73b4ff80ce7b468bd9d0add1a28ede6141bb238b
Grzegorz Ilnicki <forestgril@yahoo.com>
2019-03-08 01:37:29 +0100

    Initial import

---
+++ README.md
@@ -0,0 +1,1 @@
+Descripe what FeliksEngine is supposed to do. 
---
+++ src/feliksengine.cpp
@@ -0,0 +1,8 @@
+#include "headers/std_lib_facilities.h"
+
+int main ( int argc, char** argv ) {
+    //your code goes here
+    cout <<  "C++ with KDevelop is fun!" << endl;
+
+    return ( 0 );
+}
